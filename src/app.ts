3dddcb619dba40b69aa3e53819f8082b283bfd8a
NOMAN1802 <mustakimnoman82@gmail.com>
2024-09-29 12:54:08 +0600

    middleware error and auth related functionality done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,44 @@
-import express, { Request, Response } from "express";
-const app = express()
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import config from './app/config';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import routes from './app/routes';
 
 
-app.get('/', (req :Request, res: Response) => {
-  res.send('👍 Welcome to Gardening-Tips!')
-})
+const app: Application = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [config.client_url as string],
+  })
+);
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', routes);
+
+
+//Testing
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: 'Welcome to 🌸 Gardening Tips ',
+  });
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle not found
+app.use(notFound);
 
 export default app;
